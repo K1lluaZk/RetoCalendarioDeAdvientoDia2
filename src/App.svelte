f86ad974d77fdeeb98f98e254190b09560b58d41
Mario Suero <159667524+K1lluaZk@users.noreply.github.com>
2025-12-06 18:03:30 -0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  let name = "Diciembre";
+</script>
+
+<main>
+  <h1>¡Hola {name}! 🎉</h1>
+  <p>Feliz Navidad</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    margin-top: 50px;
+    font-family: 'Cascadia Code', sans-serif;
+    background: #0b1d3a; /* azul oscuro */
+    color: #f0f0f0;      /* texto claro para contraste */
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 20px;
+    color: #4da6ff; /* azul más claro para resaltar */
+  }
+
+  p {
+    font-size: 1.2rem;
+    color: #cce6ff; /* tono suave para el párrafo */
+  }
+</style>
